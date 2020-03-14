fd7d7371e63c80b5fbeb901a24e58ba9efa60ff4
SolvemyProblem <namthanhnguyen@github.com>
2020-03-14 11:09:24 +0700

    Sum

---
+++ TinhTongDaySo.java
@@ -0,0 +1,20 @@
+package Sum;
+
+public class TinhTongDaySo {
+    private int doDaiDay;
+
+    public TinhTongDaySo(int doDaiDay) {
+        this.doDaiDay = doDaiDay;
+    }
+
+    public TinhTongDaySo() {
+    }
+
+    public int getDoDaiDay() {
+        return doDaiDay;
+    }
+
+    public void setDoDaiDay(int doDaiDay) {
+        this.doDaiDay = doDaiDay;
+    }
+}
